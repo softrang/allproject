0962a2674beef01d193f095208714c4ec59fe0f4
softrang <152640876+softrang@users.noreply.github.com>
2023-12-13 19:14:50 +0600

    first commit

---
+++ css/index.js
@@ -0,0 +1,43 @@
+function storeData(v){
+    var txtInput= document.getElementById('txtInput');
+    var oldvalue=txtInput.value;
+    var t= oldvalue.length;
+    var lastChr=oldvalue[t-1];
+   txtInput.value= oldvalue+ v;
+   var oprSym =["+","-","*","/",".","%"];
+   if(oprSym.includes(v)){
+             if(oprSym.includes(lastChr)){
+                  var removeLast=oldvalue.slice(0, -1);
+                     txtInput.value= removeLast+ v; 
+
+
+                }
+            else {
+
+                     txtInput.value= oldvalue+ v;
+
+                 }
+
+   }
+
+   else {
+
+         txtInput.value= oldvalue+ v;
+
+        }
+
+}
+
+
+function calData(){
+
+    var txtInput= document.getElementById('txtInput');
+    var oldvalue=eval(txtInput.value);
+    txtInput.value = oldvalue
+}
+
+function clearData(){
+
+    var txtInput= document.getElementById('txtInput').value= '';
+}
+
